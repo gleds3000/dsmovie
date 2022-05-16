8d066bd0d75aeb37a894968afe27f0c903f651f2
Gleds3000 <gleds3000@gmail.com>
2022-05-15 22:14:07 -0300

    Domain model, database seed

---
+++ backend/dsmovie/src/main/java/com/gleds3000/dsmovie/entities/Movie.java
@@ -0,0 +1,67 @@
+package com.gleds3000.dsmovie.entities;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "tb_movie")
+public class Movie {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String title;
+    private String score;
+    private Integer count ;
+    private String image;
+
+    public Movie(){
+    }
+
+    public Movie(Long id, String title, String score, Integer count, String image) {
+        this.id = id;
+        this.title = title;
+        this.score = score;
+        this.count = count;
+        this.image = image;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getScore() {
+        return score;
+    }
+
+    public void setScore(String score) {
+        this.score = score;
+    }
+
+    public Integer getCount() {
+        return count;
+    }
+
+    public void setCount(Integer count) {
+        this.count = count;
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
+    }
+}
