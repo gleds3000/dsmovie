6b59e53450075a0e17a25ee1a823884c2a911741
Gleds3000 <gleds3000@gmail.com>
2023-05-22 22:18:32 -0300

    Configurando Opentelemetry Jaeger

--- backend/dsmovie/src/main/java/com/gleds3000/dsmovie/DsmovieApplication.java
+++ backend/dsmovie/src/main/java/com/gleds3000/dsmovie/DsmovieApplication.java
@@ -1,7 +1,10 @@
 package com.gleds3000.dsmovie;
 
+import io.jaegertracing.Configuration;
+import io.jaegertracing.internal.samplers.ProbabilisticSampler;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class DsmovieApplication {
--- backend/dsmovie/src/main/java/com/gleds3000/dsmovie/controllers/MovieController.java
+++ backend/dsmovie/src/main/java/com/gleds3000/dsmovie/controllers/MovieController.java
@@ -2,6 +2,8 @@ package com.gleds3000.dsmovie.controllers;
 
 import com.gleds3000.dsmovie.dto.MovieDTO;
 import com.gleds3000.dsmovie.services.MovieService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -13,17 +15,22 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping(value = "/movies")
 public class MovieController {
-
+    private static final Logger LOGGER = LoggerFactory.getLogger(MovieController.class);
     @Autowired
     private MovieService service;
 
     @GetMapping
     public Page<MovieDTO> findAll(Pageable pageable){
+
         return service.findAll(pageable);
     }
 
     @GetMapping(value = "/{id}" )
     public MovieDTO findById(@PathVariable Long id){
+
+        LOGGER.info("Detalhes do filme por Id {}", id);
+
+
         return service.findById(id);
     }
 }
